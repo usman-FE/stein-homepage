5b3739dfbb9b0b01ee2a8825c09edeb4b7772282
Muhammad Usman <ch.usman564@gmail.com>
2022-07-09 17:04:20 +0500

    Build structure for header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
+    extend: {
+      colors: {
+        // PRIMARY COLORS
+        primarySky: '#4299e1',
+      },
+    },
   },
   plugins: [],
 };
