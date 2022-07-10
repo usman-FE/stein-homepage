643eaa7457de9d4a8f84171a2bded8beb5848727
Muhammad Usman <ch.usman564@gmail.com>
2022-07-10 15:56:39 +0500

    Build Complete navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ['./index.html'],
   theme: {
+    groups: ['nested-l1'],
     extend: {
       colors: {
         // PRIMARY COLORS
         primarySky: '#4299e1',
+        secondaryDark: '#222225',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant, theme }) => {
+      const groups = theme('groups') || []
+
+      groups.forEach((group) => {
+        addVariant(`group-${group}-hover`, () => {
+          return `:merge(.group-${group}):hover &`
+        })
+      })
+    })
+  ]
 };
