64fcdb5a3b40390de83d101f2cf19c9f0aad1f8b
Usman-FE <ch.usman564@gmail.com>
2022-07-20 11:24:35 +0500

    independent of config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,7 @@
-const plugin = require('tailwindcss/plugin');
-
 module.exports = {
   content: ['./index.html'],
   theme: {
-    fontFamily: {
-      IBM: ['"IBM Plex Serif"', 'serif'],
-      roboto: ['Roboto', 'sans-serif']
-    },
-    groups: ['nested-l1'],
-    extend: {
-      colors: {
-        // PRIMARY COLORS
-        primarySky: '#4299e1',
-        secondaryDark: '#222225',
-        bgDark: '#1a1a1c',
-        bgLightDark: '#2a2a2c',
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    plugin(({ addVariant, theme }) => {
-      const groups = theme('groups') || []
-
-      groups.forEach((group) => {
-        addVariant(`group-${group}-hover`, () => {
-          return `:merge(.group-${group}):hover &`
-        })
-      })
-    })
-  ]
+  plugins: [],
 };
